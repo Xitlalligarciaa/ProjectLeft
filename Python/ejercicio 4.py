2bfb9db26fa6e65f8ae23b222ea3f7491f1f5e5d
Xitlalligarciaa <xitlalligarcia2705@gmail.com>
2023-03-01 12:01:52 -0600

    Ejercicio 3 y 4

---
+++ Python/ejercicio 4.py
@@ -0,0 +1,17 @@
+#pedir tres numeros y decir cual es el mayot
+
+int numero1
+int numero2
+int numero3
+
+numero1 = input("Ingresa el primer numero", numero1)
+numero2 = input("Ingresa el segundo numero", numero2)
+numero3 = input("Ingresar el tercer numero", numero3)
+
+if 1 > 2 && 1 > 3:
+    print ("El uno es mayor")
+if 2 > 1 && 2 > 3:
+    print ("El dos es mayor")
+if 3 > 1 && 3 > 2:
+    print ("El tres es mayor")
+           
---
+++ Python/ejercicio3.py
@@ -0,0 +1,19 @@
+#desarrollar un programa donde le pida al usuario su nombre su
+#edad y pedir el precio de su carro y calcular la tenencia 1.3%
+str nombre
+int edad
+float carro
+tenencia = 0.013
+
+nombre = str ("Nombre: ")
+nombre = int(nombre)
+edad = int ("Edad: ")
+edad = int (edad)
+carro = float("ingresa el precio de tu coche")
+carro = int (carro)
+
+tenencia = carro * 0.013
+
+
+
+
