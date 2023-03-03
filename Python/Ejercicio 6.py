3cd9dd3ae6f9562b86a50d24a3c93398cf92d9e4
Xitlalligarciaa <xitlalligarcia2705@gmail.com>
2023-03-03 11:22:11 -0600

    Ejercicio 5 y 6

---
+++ Python/Ejercicio 6.py
@@ -0,0 +1,7 @@
+radio = input ("Ingresa el radio del circulo: ")
+radio = int (radio)
+
+radios = radio * radio
+
+area = 3.1416 * radios
+print ("El area es: ", area)
---
+++ Python/Ejercico 5.py
@@ -0,0 +1,8 @@
+base = input ("Ingresa la base del triangulo: ")
+base = int (base)
+altura = input ("Ingresa la altura del triangulo: ")
+altura = int (altura)
+
+alturaa = base * altura / 2
+
+print ("El area es: ", alturaa)
