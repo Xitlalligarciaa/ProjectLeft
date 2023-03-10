6e88628f2636a20e8f14db621c3c860b7d08fcdb
Xitlalligarciaa <xitlalligarcia2705@gmail.com>
2023-03-10 10:29:35 -0600

    Ejercicios de examen

---
+++ Examen ejercicios/problemas.java
@@ -0,0 +1,168 @@
+import java.util.Scanner;
+
+public class problemas {
+    public static void main(String[] args) {
+       /* Scanner scanner = new Scanner(System.in);
+        int numero1;
+        int numero2;
+        System.out.println("Ingresa un numero");
+        numero1 = scanner.nextInt();
+        System.out.println("Ingresa un numero");
+        numero2 = scanner.nextInt();
+
+
+            for (int i = numero1 ; i <= numero2 ; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }System.out.printf( "Es par" );
+        }
+        else{
+            System.out.printf( "Es impar" );
+        }
+        import java.util.Scanner;
+
+        public class Main {
+            public static void main(String[] args) {
+
+                Scanner Scanner = new Scanner(System.in);
+                int Palindrome(int num)
+                {
+                    int n = num;
+                    int rev = 0;
+
+                    while no sirve
+                    {
+                        int r = n % 10;
+
+                        rev = rev * 10 + r;
+
+                        n = n / 10;
+                    }
+                    return (num == rev);
+
+                    Scanner scanner = new scanner (system.in)
+                    int numero1;
+                    int numero2;
+                    System.out.printl("Ingresa un numero entero"
+                            numero1 = scanner.nextLine();
+                    System.out.printl("Ingresa un segundo numero entero"
+                            numero2 = scanner.nextLine();
+
+
+                    if (numero1 > numero2){
+                        System.out.printl("El numero"+numero1"es mayor ")
+                    }
+                    if (numero2 > numero2){
+                        System.out.printl("El numero"+numero1"es mayor")
+                    }
+
+                    Scanner scanner = new scanner (system.in)
+                    int numero;
+
+                    System.out.printl("Ingresa un numero enetro"
+                            numero1 = scanner.nextLine();
+
+
+                    if (numero < 0){
+                        System.out.printl("El numero es negativo"
+                    }
+                    if (numero > 0){
+                        System.out.printl("El numero positivo"
+                    }
+                    if (numero = 0){
+                        System.out.printl("El numero es cero")
+                    }
+
+                    Scanner scanner = new scanner (system.in)
+                    char caracter;
+
+                    System.out.printl("Ingresa una letra"
+                            caracter = scanner.nextChar();
+
+                    if (caracter = a, e, i, o, u){
+                    System.out.printl("El caracter es una vocal"
+                }else{
+                    System.out.printl("El caracter no es vocal"
+                }
+
+
+                    Scanner scanner = new scanner (system.in)
+                    int numero1;
+                    int numero2;
+                    int numero3;
+
+                    System.out.printl("Ingresa un numero entero"
+                            numero1 = scanner.nextLine();
+                    System.out.printl("Ingresa un segundo numero entero"
+                            numero2 = scanner.nextLine();
+                    System.out.printl("Ingresa un tercer numero entero"
+                            numero3 = scanner.nextLine();
+
+
+                    if (numero1 > numero2 && numero1 > numero3){
+                        System.out.printl("El numero"+numero1"es mayor ")
+                    }
+                    if (numero2 > numero1 && numero2 > numero3){
+                        System.out.printl("El numero"+numero2"es mayor")
+                    }
+                    if (numero3 > numero1 && numero3 > numero2){
+                        System.out.printl("El numero"+numero3"es mayor")
+
+                        4.Escribe un programa en Java que tome un número entero y determine si es primo o no.
+                                Scanner obtenerNumero = new Scanner(System.in);
+                        int contador,I,numero;
+
+                        System.out.print("Ingresa un numero: ");
+                        numero = obtenerNumero.nextInt();
+
+                        contador = 0;
+
+                        for(I = 1; I <= numero; I++)
+                        {
+                            if((numero % I) == 0)
+                            {
+                                contador++;
+                            }
+                        }
+
+                        if(contador <= 2)
+                        {
+                            System.out.println("El numero es primo");
+                        }else{
+                            System.out.println("El numero no es primo");
+                        }
+
+                        //5.Escribe un programa en Java que tome un número entero y determine si es divisible por 3 o no.
+                        Scanner reader = new Scanner(System.in);
+                        int numero1,numero2;
+
+                        System.out.println("Introduce un número");
+                        numero1 = reader.nextInt();
+
+                        System.out.println("Introduce el número por el cual quieres saber si es divisible");
+                        numero2 = reader.nextInt();
+                        if (numero1%numero2 == 0)
+                            System.out.println(numero1 + " es divisible por " + numero2);
+                        else
+                            System.out.println(numero1 + " NO es divisible por " + numero2);
+
+                        //6.Escribe un programa en Java que tome un número entero y determine si es un número perfecto o no.
+                        int i, suma = 0, n;
+                        Scanner sc = new Scanner(System.in);
+                        System.out.println("Introduce un número: ");
+                        n = sc.nextInt();
+                        for (i = 1; i < n; i++) { // i son los divisores. Se divide desde 1 hasta n-1
+                            if (n % i == 0) {
+                                suma = suma + i; // si es divisor se suma
+                            }
+                        }
+                        if (suma == n) { // si el numero es igual a la suma de sus divisores es perfecto
+                            System.out.println("Perfecto");
+                        } else {
+                            System.out.println("No es perfecto");
+
+                        }*/
+    }
+
+    }
+
--- Python/ejercicio3.py
+++ Python/ejercicio3.py
@@ -1,9 +1,6 @@
 #desarrollar un programa donde le pida al usuario su nombre su
 #edad y pedir el precio de su carro y calcular la tenencia 1.3%
-str nombre
-int edad
-float carro
-tenencia = 0.013
+
 
 nombre = str ("Nombre: ")
 nombre = int(nombre)
